902499b2e0c8dc6bcbe97954fb98097bcdd1d36b
yyq <3531236823@qq.com>
2025-04-06 16:16:47 +0800

    first commit

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP PREEMPT Sun Apr 6 16:14:12 CST 2025"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "localhost"
+#define LINUX_COMPILER "Pdx clang version 11.0.0 (https://mirrors.tuna.tsinghua.edu.cn/git/AOSP/toolchain/llvm-project b397f81060ce6d701042b782172ed13bee898b79)"
